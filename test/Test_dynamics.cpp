5af93016b08a6f504b10fec656589037a26e21e6
Grufoony <gregorio_2000@hotmail.it>
2025-01-14 13:55:09 +0100

    Will this fix the test?

--- test/Test_dynamics.cpp
+++ test/Test_dynamics.cpp
@@ -654,7 +654,7 @@ TEST_CASE("Dynamics") {
       nodes.at(4)->setCoords({1., 0.});
       graph2.buildStreetAngles();
 
-      Dynamics dynamics{graph2, 69};
+      Dynamics dynamics{graph2, 42};
 
       std::vector<uint32_t> destinationNodes{0, 2, 3, 4};
       dynamics.setDestinationNodes(destinationNodes);
