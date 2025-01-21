e5d95fc516de0ee969ad405a0b1daa7ca16f9b43
Grufoony <gregorio_2000@hotmail.it>
2025-01-21 10:12:54 +0100

    Virtual functions and docs

--- src/dsm/headers/Road.hpp
+++ src/dsm/headers/Road.hpp
@@ -15,21 +15,21 @@ namespace dsm {
     std::string m_name;
 
   public:
-    /// @brief Construct a new Street object starting from an existing street
-    /// @details The new street has different id but same capacity, length, speed limit, and node pair as the
-    ///          existing street.
-    /// @param Road The existing street
-    /// @param id The new street's id
+    /// @brief Construct a new Road object starting from an existing road
+    /// @details The new road has different id but same capacity, length, speed limit, and node pair as the
+    ///          existing road.
+    /// @param Road The existing road
+    /// @param id The new road's id
     Road(Id id, const Road&);
-    /// @brief Construct a new Street object
-    /// @param id The street's id
-    /// @param nodePair The street's node pair
-    /// @param length The street's length, in meters (default is the mean vehicle length)
-    /// @param nLanes The street's number of lanes (default is 1)
-    /// @param maxSpeed The street's speed limit, in m/s (default is 50 km/h)
-    /// @param name The street's name (default is an empty string)
-    /// @param capacity The street's capacity (default is the maximum number of vehicles that can fit in the street)
-    /// @param transportCapacity The street's transport capacity (default is 1)
+    /// @brief Construct a new Road object
+    /// @param id The road's id
+    /// @param nodePair The road's node pair
+    /// @param length The road's length, in meters (default is the mean vehicle length)
+    /// @param nLanes The road's number of lanes (default is 1)
+    /// @param maxSpeed The road's speed limit, in m/s (default is 50 km/h)
+    /// @param name The road's name (default is an empty string)
+    /// @param capacity The road's capacity (default is the maximum number of vehicles that can fit in the road)
+    /// @param transportCapacity The road's transport capacity (default is 1)
     Road(Id id,
          std::pair<Id, Id> nodePair,
          double length = m_meanVehicleLength,
@@ -38,17 +38,37 @@ namespace dsm {
          std::string name = std::string(),
          std::optional<int> capacity = std::nullopt,
          int transportCapacity = 1);
-
+    /// @brief Set the mean vehicle length, in meters (default is 5)
+    /// @param meanVehicleLength The mean vehicle length
+    /// @throws std::invalid_argument If the mean vehicle length is less or equal to 0
     static void setMeanVehicleLength(double meanVehicleLength);
+    /// @brief Get the mean vehicle length
+    /// @return double The mean vehicle length
     static double meanVehicleLength();
 
+    /// @brief Set the maximum speed, in meters per second (default is 50 km/h)
+    /// @param speed The maximum speed
+    /// @throws std::invalid_argument If the speed is less or equal to 0
     void setMaxSpeed(double speed);
 
+    /// @brief Get the length, in meters
+    /// @return double The length, in meters
     double length() const;
+    /// @brief Get the maximum speed, in meters per second
+    /// @return double The maximum speed, in meters per second
     double maxSpeed() const;
+    /// @brief Get the number of lanes
+    /// @return int The number of lanes
     int nLanes() const;
+    /// @brief Get the name
+    /// @return std::string The name
     std::string name() const;
 
+    virtual void addAgent(Id agentId) = 0;
+
     virtual int nAgents() const = 0;
+    virtual int nMovingAgents() const = 0;
+    virtual int nExitingAgents() const = 0;
+    virtual double density(bool normalized = false) const = 0;
   };
 }  // namespace dsm
