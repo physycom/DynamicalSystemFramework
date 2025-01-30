3ef73b2258207a1588ed3e4a8559791ff9350548
Grufoony <gregorio.berselli@studio.unibo.it>
2025-01-30 14:24:27 +0100

    Logging

--- src/dsm/sources/Graph.cpp
+++ src/dsm/sources/Graph.cpp
@@ -340,9 +340,11 @@ namespace dsm {
           addNode<Intersection>(nodeIndex,
                                 std::make_pair(std::stod(lat), std::stod(lon)));
           if ((highway.find("in_out") != std::string::npos) || (highway.find("outgoing_only") != std::string::npos)) {
+            Logger::info(std::format("Setting node {} as an output node", nodeIndex));
             m_outputNodes.push_back(nodeIndex);
           }
           if ((highway.find("in_out") != std::string::npos) || (highway.find("incoming_only") != std::string::npos)) {
+            Logger::info(std::format("Setting node {} as an input node", nodeIndex));
             m_inputNodes.push_back(nodeIndex);
           }
         }
