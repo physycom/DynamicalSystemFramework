681a10e7d502e53881467ddaf5cb70f9180196ac
Grufoony <gregorio.berselli@studio.unibo.it>
2025-02-17 16:47:50 +0100

    Add streetMinTime function

--- src/dsm/headers/DijkstraWeights.hpp
+++ src/dsm/headers/DijkstraWeights.hpp
@@ -1,4 +1,3 @@
-
 #pragma once
 
 #include "../utility/Typedef.hpp"
@@ -7,7 +6,25 @@ namespace dsm {
 
   class Graph;
 
+  /// @brief Returns the length of a street given its source and destination nodes
+  /// @param graph A pointer to the graph
+  /// @param node1 The source node id
+  /// @param node2 The destination node id
+  /// @return The length of the street
   double streetLength(const Graph* graph, Id node1, Id node2);
+  /// @brief Returns the minimum time to cross a street given its source and destination nodes
+  /// @param graph A pointer to the graph
+  /// @param node1 The source node id
+  /// @param node2 The destination node id
+  /// @return The minimum time to cross the street
+  /// @details The minimum time is the ratio between the street's length and the maximum speed
+  double streetMinTime(const Graph* graph, Id node1, Id node2);
+  /// @brief Returns the time to cross a street given its source and destination nodes
+  /// @param graph A pointer to the graph
+  /// @param node1 The source node id
+  /// @param node2 The destination node id
+  /// @return The time to cross the street
+  /// @details This time also takes into account the number of agents on the street
   double streetTime(const Graph* graph, Id node1, Id node2);
 
 };  // namespace dsm
--- src/dsm/sources/DijkstraWeights.cpp
+++ src/dsm/sources/DijkstraWeights.cpp
@@ -9,6 +9,14 @@ namespace dsm {
     return (*street)->length();
   }
 
+  double streetMinTime(const Graph* graph, Id node1, Id node2) {
+    const auto street{graph->street(node1, node2)};
+    const auto length{(*street)->length()};
+    const auto maxSpeed{(*street)->maxSpeed()};
+    // Logger::info(std::format("Street {}-{}: length = {}, speed = {}, time = {}", node1, node2, length, maxSpeed, length / maxSpeed));
+    return length / maxSpeed;
+  }
+
   double streetTime(const Graph* graph, Id node1, Id node2) {
     const auto street{graph->street(node1, node2)};
     const auto length{(*street)->length()};
