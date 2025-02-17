4dfa402e92bcc0a7ccf7f58c5e180d52411ef94a
Grufoony <gregorio.berselli@studio.unibo.it>
2025-02-17 17:24:19 +0100

    Add `weight_functions` namespace for clarity

--- src/dsm/sources/DijkstraWeights.cpp
+++ src/dsm/sources/DijkstraWeights.cpp
@@ -4,26 +4,19 @@
 
 namespace dsm {
 
-  double streetLength(const Graph* graph, Id node1, Id node2) {
-    const auto street{graph->street(node1, node2)};
-    return (*street)->length();
-  }
+  namespace weight_functions {
+    double streetLength(const Graph* graph, Id node1, Id node2) {
+      const auto street{graph->street(node1, node2)};
+      return (*street)->length();
+    }
 
-  double streetMinTime(const Graph* graph, Id node1, Id node2) {
-    const auto street{graph->street(node1, node2)};
-    const auto length{(*street)->length()};
-    const auto maxSpeed{(*street)->maxSpeed()};
-    // Logger::info(std::format("Street {}-{}: length = {}, speed = {}, time = {}", node1, node2, length, maxSpeed, length / maxSpeed));
-    return length / maxSpeed;
-  }
-
-  double streetTime(const Graph* graph, Id node1, Id node2) {
-    const auto street{graph->street(node1, node2)};
-    const auto length{(*street)->length()};
-    const auto speed{(*street)->maxSpeed() *
-                     (1. - (*street)->nAgents() / (*street)->capacity())};
-
-    return length / speed;
-  }
+    double streetTime(const Graph* graph, Id node1, Id node2) {
+      const auto street{graph->street(node1, node2)};
+      const auto length{(*street)->length()};
+      const auto speed{(*street)->maxSpeed() *
+                       (1. - (*street)->nAgents() / (*street)->capacity())};
+      return length / speed;
+    }
+  }  // namespace weight_functions
 
 };  // namespace dsm
