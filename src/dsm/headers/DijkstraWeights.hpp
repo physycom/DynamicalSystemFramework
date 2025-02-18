1848e8377c8b27dda20630ec9a1cca2cad1ae597
Gregorio Berselli <gregorio.berselli@studio.unibo.it>
2025-02-18 09:34:55 +0100

    Add possibility to use travel time as weight for `updatePaths` (#268)
    * Add streetMinTime function
    
    * Add `weight_functions` namespace for clarity
    
    * Add and test possibility to change weight function in update paths
    
    * Add and test possibility to change weight threshold
    
    * Update version

--- src/dsm/headers/DijkstraWeights.hpp
+++ src/dsm/headers/DijkstraWeights.hpp
@@ -1,4 +1,3 @@
-
 #pragma once
 
 #include "../utility/Typedef.hpp"
@@ -7,7 +6,20 @@ namespace dsm {
 
   class Graph;
 
-  double streetLength(const Graph* graph, Id node1, Id node2);
-  double streetTime(const Graph* graph, Id node1, Id node2);
+  namespace weight_functions {
+    /// @brief Returns the length of a street given its source and destination nodes
+    /// @param graph A pointer to the graph
+    /// @param node1 The source node id
+    /// @param node2 The destination node id
+    /// @return The length of the street
+    double streetLength(const Graph* graph, Id node1, Id node2);
+    /// @brief Returns the time to cross a street given its source and destination nodes
+    /// @param graph A pointer to the graph
+    /// @param node1 The source node id
+    /// @param node2 The destination node id
+    /// @return The time to cross the street
+    /// @details This time also takes into account the number of agents on the street
+    double streetTime(const Graph* graph, Id node1, Id node2);
+  }  // namespace weight_functions
 
 };  // namespace dsm
