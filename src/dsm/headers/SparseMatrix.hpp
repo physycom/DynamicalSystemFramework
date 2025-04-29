23e49c672ba217b309fde4bdc62152b82e1bc267
Grufoony <gregorio.berselli@studio.unibo.it>
2025-04-29 15:27:40 +0200

    End SparseMatrix implementation

--- src/dsm/headers/SparseMatrix.hpp
+++ src/dsm/headers/SparseMatrix.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <algorithm>
 #include <cassert>
+#ifndef __APPLE__
+#include <execution>
+#endif
+#include <format>
+#include <fstream>
+#include <map>  // A flat map would be better
 #include <stdexcept>
 #include <string>
 #include <vector>
-
-#include <tbb/parallel_for_each.h>
 
 #include "../utility/Typedef.hpp"
 
@@ -21,6 +26,7 @@ namespace dsm {
     std::vector<Id> m_columnIndices;
     std::vector<T> m_values;
     size_t m_n;
+    T m_TdefaultValue = static_cast<T>(0);
 
   public:
     /// @brief Construct a new SparseMatrix object
@@ -29,22 +35,50 @@ namespace dsm {
     /// @param fileName The name of the file containing the sparse matrix
     SparseMatrix(std::string const& fileName);
 
+    /// @brief Returns the element at the specified row and column
+    /// @param row The row index of the element
+    /// @param col The column index of the element
+    /// @return The element at the specified row and column
     T operator()(Id row, Id col) const;
+
+    bool operator==(const SparseMatrix& other) const;
 
     /// @brief Returns the number of (non-zero) elements in the matrix
     /// @return The number of elements in the matrix
-    size_t size() const { return m_values.size(); }
+    inline size_t size() const noexcept { return m_values.size(); }
     /// @brief Returns true if the matrix is empty
     /// @return true if the matrix is empty, false otherwise
-    bool empty() const { return m_values.empty(); }
+    inline bool empty() const noexcept { return m_values.empty(); }
     /// @brief Returns the number of rows (columns) in the matrix
     /// @return The number of rows (columns) in the matrix
-    size_t n() const { return m_n; }
+    inline size_t n() const noexcept { return m_n; }
+    /// @brief Returns a row of the matrix as a map of column indices and values
+    /// @param idRow The row index
+    /// @return A map of column indices and values for the specified row
+    std::map<Id, T> row(Id idRow) const;
+    /// @brief Returns a column of the matrix as a map of row indices and values
+    /// @param idCol The column index
+    /// @return A map of row indices and values for the specified column
+    std::map<Id, T> col(Id idCol) const;
 
+    /// @brief Set the default value of the matrix (default is 0)
+    /// @param value The default value of the matrix
+    inline void setDefaultValue(T value) { m_TdefaultValue = value; }
+    /// @brief Insert a new element of given value at the specified row and column
+    /// @param row The row index of the element
+    /// @param col The column index of the element
+    /// @param value The value of the element
+    /// @details This function actually inserts element \f$a_{ij}\f$ of the sparse matrix.
+    ///   Where \f$i\f$ is the row index and \f$j\f$ is the column index.
+    ///   The function will automatically resize the matrix to fit the new element.
     void insert(Id row, Id col, T value);
-
+    /// @brief Read the sparse matrix from a binary file
+    /// @param fileName The name of the file containing the sparse matrix
+    /// @throw std::runtime_error if the file cannot be opened
     void read(std::string const& fileName);
-
+    /// @brief Save the sparse matrix to a binary file
+    /// @param fileName The name of the file to save the sparse matrix
+    /// @throw std::runtime_error if the file cannot be opened
     void save(std::string const& fileName) const;
   };
 
@@ -55,7 +89,6 @@ namespace dsm {
     requires(std::is_arithmetic_v<T>)
   SparseMatrix<T>::SparseMatrix()
       : m_rowOffsets{std::vector<Id>(2, 0)}, m_columnIndices{}, m_values{}, m_n{1} {}
-
   template <typename T>
     requires(std::is_arithmetic_v<T>)
   SparseMatrix<T>::SparseMatrix(std::string const& fileName) {
@@ -75,11 +108,28 @@ namespace dsm {
     auto itLast = m_columnIndices.begin() + m_rowOffsets[row + 1];
     auto it = std::find(itFirst, itLast, col);
     if (it == itLast) {
-      return static_cast<T>(0);  // Return default value if not found
+      return m_TdefaultValue;  // Return default value if not found
     }
     size_t const index = std::distance(itFirst, it);
     assert(index < m_values.size());
     return m_values[index];
+  }
+  template <typename T>
+    requires(std::is_arithmetic_v<T>)
+  bool SparseMatrix<T>::operator==(const SparseMatrix& other) const {
+    if (m_n != other.m_n) {
+      return false;
+    }
+    if (m_rowOffsets != other.m_rowOffsets) {
+      return false;
+    }
+    if (m_columnIndices != other.m_columnIndices) {
+      return false;
+    }
+    if (m_values != other.m_values) {
+      return false;
+    }
+    return true;
   }
   /*********************************************************************************
      * METHODS
@@ -98,12 +148,91 @@ namespace dsm {
     assert(row + 1 < m_rowOffsets.size());
 
     // Increase row offsets for rows after the inserted row
-    tbb::parallel_for_each(
-        m_rowOffsets.begin() + row + 1, m_rowOffsets.end(), [](Id& x) { x++; });
+    std::for_each(
+#ifndef __APPLE__
+        std::execution::par_unseq,
+#endif
+        m_rowOffsets.begin() + row + 1,
+        m_rowOffsets.end(),
+        [](Id& x) { x++; });
 
     // Insert column index at the correct position
     auto csrOffset = m_rowOffsets[row + 1] - 1;
     m_columnIndices.insert(m_columnIndices.begin() + csrOffset, col);
     m_values.insert(m_values.begin() + csrOffset, value);
+  }
+
+  template <typename T>
+    requires(std::is_arithmetic_v<T>)
+  std::map<Id, T> SparseMatrix<T>::row(Id idRow) const {
+    if (idRow >= m_n) {
+      throw std::out_of_range(std::format("Row index ({}) out of range.", idRow));
+    }
+    std::map<Id, T> rowMap;
+    if (idRow + 1 >= m_rowOffsets.size()) {
+      return rowMap;
+    }
+    const auto lowerOffset = m_rowOffsets[idRow];
+    const auto upperOffset = m_rowOffsets[idRow + 1];
+    for (auto i = lowerOffset; i < upperOffset; ++i) {
+      rowMap[m_columnIndices[i]] = m_values[i];
+    }
+    return rowMap;
+  }
+  template <typename T>
+    requires(std::is_arithmetic_v<T>)
+  std::map<Id, T> SparseMatrix<T>::col(Id idCol) const {
+    if (idCol >= m_n) {
+      throw std::out_of_range(std::format("Column index ({}) out of range.", idCol));
+    }
+    std::map<Id, T> colMap;
+    for (Id row = 0; row + 1 < m_rowOffsets.size(); ++row) {
+      auto lowerOffset = m_rowOffsets[row];
+      auto upperOffset = m_rowOffsets[row + 1];
+
+      for (auto i = lowerOffset; i < upperOffset; ++i) {
+        if (m_columnIndices[i] == idCol) {
+          colMap[row] = m_values[i];
+          break;  // Since column indices are assumed to be sorted within a row
+        }
+      }
+    }
+    return colMap;
+  }
+  template <typename T>
+    requires(std::is_arithmetic_v<T>)
+  void SparseMatrix<T>::read(std::string const& fileName) {
+    std::ifstream inStream(fileName, std::ios::binary);
+    if (!inStream.is_open()) {
+      throw std::runtime_error(
+          std::format("Could not open file \'{}\' for reading.", fileName));
+    }
+    inStream.read(reinterpret_cast<char*>(&m_n), sizeof(size_t));
+    m_rowOffsets.resize(m_n + 1);
+    inStream.read(reinterpret_cast<char*>(m_rowOffsets.data()),
+                  m_rowOffsets.size() * sizeof(Id));
+    m_columnIndices.resize(m_rowOffsets.back());
+    inStream.read(reinterpret_cast<char*>(m_columnIndices.data()),
+                  m_columnIndices.size() * sizeof(Id));
+    m_values.resize(m_rowOffsets.back());
+    inStream.read(reinterpret_cast<char*>(m_values.data()), m_values.size() * sizeof(T));
+    inStream.close();
+  }
+  template <typename T>
+    requires(std::is_arithmetic_v<T>)
+  void SparseMatrix<T>::save(std::string const& fileName) const {
+    std::ofstream outStream(fileName, std::ios::binary);
+    if (!outStream.is_open()) {
+      throw std::runtime_error(
+          std::format("Could not open file \'{}\' for writing.", fileName));
+    }
+    outStream.write(reinterpret_cast<const char*>(&m_n), sizeof(size_t));
+    outStream.write(reinterpret_cast<const char*>(m_rowOffsets.data()),
+                    m_rowOffsets.size() * sizeof(Id));
+    outStream.write(reinterpret_cast<const char*>(m_columnIndices.data()),
+                    m_columnIndices.size() * sizeof(Id));
+    outStream.write(reinterpret_cast<const char*>(m_values.data()),
+                    m_values.size() * sizeof(T));
+    outStream.close();
   }
 }  // namespace dsm
--- test/Test_SparseMatrix.cpp
+++ test/Test_SparseMatrix.cpp
@@ -11,6 +11,7 @@ TEST_CASE("Double matrix") {
     CHECK(mat.empty());
     CHECK_EQ(mat.size(), 0);
     CHECK_EQ(mat.n(), 1);
+    CHECK_EQ(mat(0, 0), 0.0);
     mat.insert(0, 1, 1.0);
     CHECK_FALSE(mat.empty());
     CHECK_EQ(mat.size(), 1);
@@ -20,5 +21,60 @@ TEST_CASE("Double matrix") {
     CHECK_EQ(mat(0, 0), 0.0);
     CHECK_EQ(mat(1, 1), 0.0);
     CHECK_THROWS_AS(mat(0, 2), std::out_of_range);
+    mat.setDefaultValue(6.9);
+    CHECK_EQ(mat(0, 0), 6.9);
+  }
+  Matrix testMat;
+  testMat.insert(0, 1, 1.0);
+  testMat.insert(1, 2, 2.0);
+  testMat.insert(1, 3, 3.0);
+  testMat.insert(2, 3, 4.0);
+  testMat.insert(3, 4, 5.0);
+  testMat.insert(0, 0, 6.0);
+  SUBCASE("Save and read") {
+    auto path = "./data/sparse_matrix.dsm";
+    testMat.save(path);
+    Matrix mat(path);
+    CHECK_EQ(testMat, mat);
+  }
+  SUBCASE("row getter") {
+    std::map<Id, double> row;
+    row = testMat.row(0);
+    CHECK_EQ(row.size(), 2);
+    CHECK_EQ(row[0], 6.0);
+    CHECK_EQ(row[1], 1.0);
+    row = testMat.row(1);
+    CHECK_EQ(row.size(), 2);
+    CHECK_EQ(row[2], 2.0);
+    CHECK_EQ(row[3], 3.0);
+    row = testMat.row(2);
+    CHECK_EQ(row.size(), 1);
+    CHECK_EQ(row[3], 4.0);
+    row = testMat.row(3);
+    CHECK_EQ(row.size(), 1);
+    CHECK_EQ(row[4], 5.0);
+    row = testMat.row(4);
+    CHECK(row.empty());
+    CHECK_THROWS_AS(testMat.row(5), std::out_of_range);
+  }
+  SUBCASE("col getter") {
+    std::map<Id, double> col;
+    col = testMat.col(0);
+    CHECK_EQ(col.size(), 1);
+    CHECK_EQ(col[0], 6.0);
+    col = testMat.col(1);
+    CHECK_EQ(col.size(), 1);
+    CHECK_EQ(col[0], 1.0);
+    col = testMat.col(2);
+    CHECK_EQ(col.size(), 1);
+    CHECK_EQ(col[1], 2.0);
+    col = testMat.col(3);
+    CHECK_EQ(col.size(), 2);
+    CHECK_EQ(col[1], 3.0);
+    CHECK_EQ(col[2], 4.0);
+    col = testMat.col(4);
+    CHECK_EQ(col.size(), 1);
+    CHECK_EQ(col[3], 5.0);
+    CHECK_THROWS_AS(testMat.col(5), std::out_of_range);
   }
 }
