466fdf9a1992c04e4ddc12a27e43f57f76d86487
Grufoony <gregorio.berselli@studio.unibo.it>
2025-09-11 14:09:36 +0200

    Fix benchmarks

--- benchmark/Adj/BenchAdj.cpp
+++ benchmark/Adj/BenchAdj.cpp
@@ -14,42 +14,39 @@ int main() {
   RoadNetwork graph{};
   graph.importOSMNodes("../test/data/forlì_nodes.csv");
   graph.importOSMEdges("../test/data/forlì_edges.csv");
-  graph.buildAdj();
-  auto const& adj{graph.adjacencyMatrix()};
-  auto const N{adj.n()};
+  auto const N{graph.nNodes()};
   SparseMatrix<bool> sm;
-  for (const auto& [srcId, dstId] : adj.elements()) {
-    sm.insert(srcId, dstId, true);
+  for (const auto& pEdge : graph.edges()) {
+    sm.insert(pEdge->source(), pEdge->target(), true);
   }
-
 
   const int n_rep{1000};
   Bench b1(n_rep), b2(n_rep), b3(n_rep), b4(n_rep);
   Logger::info("Benchmarking SparseMatrix::getCol");
-  b1.benchmark([&sm, &N]() -> void { 
+  b1.benchmark([&sm, &N]() -> void {
     for (size_t i{0}; i < N; ++i) {
       sm.col(i);
     }
   });
   b1.print<sb::microseconds>();
   Logger::info("Benchmarking SparseMatrix::getRow");
-  b2.benchmark([&sm, &N]() -> void { 
+  b2.benchmark([&sm, &N]() -> void {
     for (size_t i{0}; i < N; ++i) {
       sm.row(i);
     }
   });
   b2.print<sb::microseconds>();
   Logger::info("Benchmarking AdjacencyMatrix::getCol");
-  b3.benchmark([&adj, &N]() -> void { 
+  b3.benchmark([&graph, &N]() -> void {
     for (size_t i{0}; i < N; ++i) {
-      adj.getCol(i);
+      graph.inputNeighbors(i);
     }
   });
   b3.print<sb::microseconds>();
   Logger::info("Benchmarking AdjacencyMatrix::getRow");
-  b4.benchmark([&adj, &N]() -> void { 
+  b4.benchmark([&graph, &N]() -> void {
     for (size_t i{0}; i < N; ++i) {
-      adj.getRow(i);
+      graph.outputNeighbors(i);
     }
   });
   b4.print<sb::microseconds>();
