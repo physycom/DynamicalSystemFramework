634a9b62d6744a9223610cca86bf33f58ed738af
grufoony <gregorio_2000@hotmail.it>
2025-10-20 17:38:01 +0200

    Rewrote node tests

--- src/dsf/sources/TrafficLight.cpp
+++ src/dsf/sources/TrafficLight.cpp
@@ -1,173 +1,73 @@
 #include "../headers/TrafficLight.hpp"
 
 #include <format>
-#include <numeric>
 #include <stdexcept>
 
 #include <spdlog/spdlog.h>
 
 namespace dsf {
-  bool TrafficLightCycle::isGreen(Delay const cycleTime, Delay const counter) const {
-    auto const greenStart = m_phase % cycleTime;
-    auto const greenEnd = (m_phase + m_greenTime) % cycleTime;
-
-    if (greenStart < greenEnd) {
-      // Normal case: green does not wrap around
-      return (counter >= greenStart) && (counter < greenEnd);
-    } else {
-      // Wraparound case: green spans cycle boundary
-      return (counter >= greenStart) || (counter < greenEnd);
-    }
-  }
-
-  bool TrafficLight::m_allowFreeTurns{true};
-  void TrafficLight::setAllowFreeTurns(bool allow) { m_allowFreeTurns = allow; }
-  void TrafficLight::setCycle(Id const streetId,
-                              Direction direction,
-                              TrafficLightCycle const& cycle) {
-    if ((cycle.greenTime() > m_cycleTime)) {
-      throw std::invalid_argument(
-          std::format("Green time ({}) must not exceed the cycle time ({}).",
-                      cycle.greenTime(),
-                      m_cycleTime));
-    }
-    if (!(cycle.phase() < m_cycleTime)) {
-      throw std::invalid_argument(
-          std::format("Phase ({}) must be less than the cycle time ({}).",
-                      cycle.phase(),
-                      m_cycleTime));
-    }
-    m_cycles[streetId].emplace(direction, cycle);
-  }
-
-  void TrafficLightCycle::reset() {
-    m_greenTime = m_defaultValues.first;
-    m_phase = m_defaultValues.second;
-  }
-
-  void TrafficLight::setComplementaryCycle(Id const streetId, Id const existingCycle) {
-    if (m_cycles.contains(streetId)) {
-      throw std::invalid_argument(std::format(
-          "Street with id {} already has a cycle in traffic light with id {}.",
-          streetId,
-          m_id));
-    }
-    if (!m_cycles.contains(existingCycle)) {
-      throw std::invalid_argument(std::format(
-          "Existing cycle with id {} does not exist in traffic light with id {}.",
-          existingCycle,
-          m_id));
-    }
-    m_cycles.emplace(streetId, m_cycles.at(existingCycle));
-    for (auto& [direction, cycle] : m_cycles.at(streetId)) {
-      cycle = TrafficLightCycle(m_cycleTime - cycle.greenTime(),
-                                cycle.phase() + m_cycleTime - cycle.greenTime());
-    }
-  }
-
   TrafficLight& TrafficLight::operator++() {
     m_counter = (m_counter + 1) % m_cycleTime;
     return *this;
   }
 
-  double TrafficLight::meanGreenTime(bool priorityStreets) const {
-    double meanTime{0.};
-    size_t nCycles{0};
-    for (auto const& [streetId, cycles] : m_cycles) {
-      if ((priorityStreets && m_streetPriorities.contains(streetId)) ||
-          (!priorityStreets && !m_streetPriorities.contains(streetId))) {
-        meanTime +=
-            std::transform_reduce(cycles.begin(),
-                                  cycles.end(),
-                                  0.0,                  // Initial value (double)
-                                  std::plus<double>(),  // Reduction function (addition)
-                                  [](const auto& pair) -> double {
-                                    return static_cast<double>(pair.second.greenTime());
-                                  });
-        nCycles += cycles.size();
+  void TrafficLight::setPhases(std::vector<TrafficLightPhase> phases) {
+    Delay totalGreenTime{0};
+    for (auto const& phase : phases) {
+      totalGreenTime += phase.greenTime();
+      auto const& streetIds = phase.streetIds();
+      if (streetIds.empty()) {
+        throw std::invalid_argument(
+            std::format("Traffic light phase with green time {} has no streets assigned.",
+                        phase.greenTime()));
       }
-    }
-    return meanTime / nCycles;
-  }
-
-  void TrafficLight::increasePhases(Delay const phase) {
-    for (auto& [streetId, cycles] : m_cycles) {
-      for (auto& [direction, cycle] : cycles) {
-        // Module new phase with cycleTime
-        auto newPhase{(phase + cycle.phase()) % m_cycleTime};
-        cycle = TrafficLightCycle(cycle.greenTime(), newPhase);
-      }
-    }
-  }
-
-  bool TrafficLight::isDefault() const {
-    for (auto const& [streetId, cycles] : m_cycles) {
-      for (auto const& [direction, cycle] : cycles) {
-        if (!cycle.isDefault()) {
-          return false;
+      for (auto const& streetId : streetIds) {
+        if (std::find(m_ingoingEdges.cbegin(), m_ingoingEdges.cend(), streetId) ==
+            m_ingoingEdges.cend()) {
+          throw std::invalid_argument(std::format(
+              "Street with id {} is not ingoing edge of traffic light with id {}.",
+              streetId,
+              m_id));
         }
       }
+      if (totalGreenTime > m_cycleTime) {
+        throw std::invalid_argument(std::format(
+            "Total green time of all phases ({}) is greater than the cycle time ({}).",
+            totalGreenTime,
+            m_cycleTime));
+      }
     }
-    return true;
+    m_phases = std::move(phases);
   }
 
   bool TrafficLight::isGreen(Id const streetId, Direction direction) const {
-    if (!m_cycles.contains(streetId)) {
-      throw std::invalid_argument(
-          std::format("Street id {} is not valid for node {}.", streetId, id()));
-    }
-    if (!m_cycles.at(streetId).contains(direction)) {
-      if (m_cycles.at(streetId).contains(Direction::ANY)) {
-        direction = Direction::ANY;
-      } else {
-        switch (direction) {
-          case Direction::RIGHT:
-            if (m_cycles.at(streetId).contains(Direction::RIGHTANDSTRAIGHT)) {
-              direction = Direction::RIGHTANDSTRAIGHT;
-            } else if (m_cycles.at(streetId).contains(Direction::ANY)) {
-              direction = Direction::ANY;
-            }
-            break;
-          case Direction::LEFT:
-            if (m_cycles.at(streetId).contains(Direction::LEFTANDSTRAIGHT)) {
-              direction = Direction::LEFTANDSTRAIGHT;
-            } else if (m_cycles.at(streetId).contains(Direction::ANY)) {
-              direction = Direction::ANY;
-            }
-            break;
-          case Direction::STRAIGHT:
-            if (m_cycles.at(streetId).contains(Direction::RIGHTANDSTRAIGHT)) {
-              direction = Direction::RIGHTANDSTRAIGHT;
-            } else if (m_cycles.at(streetId).contains(Direction::LEFTANDSTRAIGHT)) {
-              direction = Direction::LEFTANDSTRAIGHT;
-            } else if (m_cycles.at(streetId).contains(Direction::ANY)) {
-              direction = Direction::ANY;
-            }
-            break;
-          case Direction::UTURN:
-            if (m_cycles.at(streetId).contains(Direction::LEFT)) {
-              direction = Direction::LEFT;
-            } else if (m_cycles.at(streetId).contains(Direction::LEFTANDSTRAIGHT)) {
-              direction = Direction::LEFTANDSTRAIGHT;
-            } else if (m_cycles.at(streetId).contains(Direction::ANY)) {
-              direction = Direction::ANY;
-            }
-            break;
-          default:
-            spdlog::warn(
-                "Street {} has ...ANDSTRAIGHT phase but Traffic Light {} doesn't.",
-                streetId,
-                m_id);
-        }
+    if (m_freeTurns.contains(streetId)) {
+      Direction const freeDirection{m_freeTurns.at(streetId)};
+      if (freeDirection == Direction::ANY || freeDirection == direction ||
+          (freeDirection == Direction::RIGHTANDSTRAIGHT &&
+           (direction == Direction::RIGHT || direction == Direction::STRAIGHT)) ||
+          (freeDirection == Direction::LEFTANDSTRAIGHT &&
+           (direction == Direction::LEFT || direction == Direction::STRAIGHT))) {
+        return true;
       }
     }
-    if (!m_cycles.at(streetId).contains(direction)) {
-      return m_allowFreeTurns;
+    Delay sumTime{0};
+    for (auto const& phase : m_phases) {
+      sumTime += phase.greenTime();
+      if (m_counter >= sumTime) {
+        continue;
+      }
+      if (phase.streetIds().contains(streetId)) {
+        if (phase.direction() == Direction::ANY || phase.direction() == direction ||
+            (phase.direction() == Direction::RIGHTANDSTRAIGHT &&
+             (direction == Direction::RIGHT || direction == Direction::STRAIGHT)) ||
+            (phase.direction() == Direction::LEFTANDSTRAIGHT &&
+             (direction == Direction::LEFT || direction == Direction::STRAIGHT))) {
+          return true;
+        }
+      }
+      return false;
     }
-    return m_cycles.at(streetId).at(direction).isGreen(m_cycleTime, m_counter);
-  }
-
-  void TrafficLight::resetCycles() {
-    m_defaultCycles.empty() ? m_defaultCycles = m_cycles : m_cycles = m_defaultCycles;
+    return false;
   }
 }  // namespace dsf
--- test/Test_node.cpp
+++ test/Test_node.cpp
@@ -9,10 +9,6 @@
 #include "doctest.h"
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-using Intersection = dsf::Intersection;
-using TrafficLight = dsf::TrafficLight;
-using Station = dsf::Station;
-
 TEST_CASE("Intersection") {
   SUBCASE("Constructor") {
     GIVEN("Some parameters") {
@@ -21,7 +17,7 @@ TEST_CASE("Intersection") {
       constexpr double lon = 3.5;
       const std::string name = "MyName";
       WHEN("An Intersection is created using only an Id") {
-        Intersection intersection{id};
+        dsf::Intersection intersection{id};
         THEN("Parameters are set correctly") {
           CHECK_EQ(intersection.id(), id);
           CHECK_FALSE(intersection.coords().has_value());
@@ -31,7 +27,7 @@ TEST_CASE("Intersection") {
         }
       }
       WHEN("An Intersection is created using an Id and coordinates") {
-        Intersection intersection{id, std::make_pair(lat, lon)};
+        dsf::Intersection intersection{id, std::make_pair(lat, lon)};
         THEN("Parameters are set correctly") {
           CHECK_EQ(intersection.id(), id);
           CHECK(intersection.coords().has_value());
@@ -65,16 +61,16 @@ TEST_CASE("Intersection") {
 TEST_CASE("TrafficLight") {
   SUBCASE("Constructor") {
     GIVEN("A traffic light object") {
-      TrafficLight tl{0, 60};
+      dsf::TrafficLight tl{0, 60};
       THEN("The traffic light is created with the correct id and cycle time") {
         CHECK_EQ(tl.id(), 0);
         CHECK_EQ(tl.cycleTime(), 60);
       }
     }
     GIVEN("A node object") {
-      Intersection intersection(0, std::make_pair(1., 2.));
+      dsf::Intersection intersection(0, std::make_pair(1., 2.));
       WHEN("The node is converted to a traffic light") {
-        TrafficLight tl(intersection, 60);
+        dsf::TrafficLight tl(intersection, 60);
         THEN("The traffic light is created with correct parameters") {
           CHECK_EQ(tl.id(), 0);
           CHECK_EQ(tl.cycleTime(), 60);
@@ -86,128 +82,61 @@ TEST_CASE("TrafficLight") {
     }
   }
   SUBCASE("Light cycle") {
-    TrafficLight::setAllowFreeTurns(true);
     GIVEN("A traffic light object with a cycle set") {
-      TrafficLight tl{0, 2};
-      tl.setCycle(0, dsf::Direction::LEFT, {1, 0});
-      WHEN("We increase counter") {
-        ++tl;
-        THEN("The traffic light is green for all except Left and U turns") {
-          CHECK(tl.isGreen(0, dsf::Direction::RIGHT));
-          CHECK(tl.isGreen(0, dsf::Direction::STRAIGHT));
-          CHECK_FALSE(tl.isGreen(0, dsf::Direction::LEFT));
-          CHECK_FALSE(tl.isGreen(0, dsf::Direction::UTURN));
+      dsf::TrafficLight tl{0, 20};
+      WHEN("We create a TrafficLightPhase") {
+        dsf::TrafficLightPhase phase{10, dsf::Direction::ANY};
+        THEN("We have to insert at least one street id") {
+          CHECK_THROWS_AS(tl.setPhases({phase}), std::invalid_argument);
         }
-        ++tl;
-        THEN("The traffic light is green for all") {
-          CHECK(tl.isGreen(0, dsf::Direction::RIGHT));
-          CHECK(tl.isGreen(0, dsf::Direction::STRAIGHT));
-          CHECK(tl.isGreen(0, dsf::Direction::LEFT));
-          CHECK(tl.isGreen(0, dsf::Direction::UTURN));
+        phase.setStreetIds({0});
+        THEN("The inserted streetId must be an ingoing edge of the traffic light") {
+          CHECK_THROWS_AS(tl.setPhases({phase}), std::invalid_argument);
+        }
+        tl.addIngoingEdge(0);
+        tl.setPhases({phase});
+        THEN("The phase is set correctly") {
+          CHECK_EQ(tl.phases().size(), 1);
+          CHECK_EQ(tl.phases().front().greenTime(), 10);
+          CHECK_EQ(tl.phases().front().direction(), dsf::Direction::ANY);
+          CHECK_EQ(tl.phases().front().streetIds().size(), 1);
+          CHECK_EQ(*(tl.phases().front().streetIds().begin()), 0);
+        }
+        dsf::TrafficLightPhase otherphase{15, dsf::Direction::RIGHT};
+        otherphase.setStreetIds({0});
+        THEN("It must have the green time not exceeding the cycle time") {
+          CHECK_THROWS_AS(tl.setPhases({phase, otherphase}), std::invalid_argument);
         }
       }
     }
     GIVEN("A traffic light object with a cycle set") {
-      TrafficLight tl{0, 3};
-      tl.setCycle(0, dsf::Direction::RIGHT, {2, 2});
+      dsf::TrafficLight tl{0, 3};
+      tl.addIngoingEdge(0);
+      tl.addFreeTurn(0, dsf::Direction::LEFTANDSTRAIGHT);
+      dsf::TrafficLightPhase phase1{2, dsf::Direction::RIGHT};
+      phase1.setStreetIds({0});
+      tl.setPhases({phase1});
       THEN("Traffic light is green for all") {
         CHECK(tl.isGreen(0, dsf::Direction::RIGHT));
         CHECK(tl.isGreen(0, dsf::Direction::STRAIGHT));
         CHECK(tl.isGreen(0, dsf::Direction::LEFT));
-        CHECK(tl.isGreen(0, dsf::Direction::UTURN));
       }
       WHEN("We increase counter") {
-        ++tl;
-        THEN("Traffic light is green for all except Right") {
-          CHECK_FALSE(tl.isGreen(0, dsf::Direction::RIGHT));
-          CHECK(tl.isGreen(0, dsf::Direction::STRAIGHT));
-          CHECK(tl.isGreen(0, dsf::Direction::LEFT));
-          CHECK(tl.isGreen(0, dsf::Direction::UTURN));
-        }
         ++tl;
         THEN("Traffic light is green for all") {
           CHECK(tl.isGreen(0, dsf::Direction::RIGHT));
           CHECK(tl.isGreen(0, dsf::Direction::STRAIGHT));
           CHECK(tl.isGreen(0, dsf::Direction::LEFT));
-          CHECK(tl.isGreen(0, dsf::Direction::UTURN));
+        }
+        ++tl;
+        THEN("Traffic light is green for all except RIGHT") {
+          CHECK_FALSE(tl.isGreen(0, dsf::Direction::RIGHT));
+          CHECK(tl.isGreen(0, dsf::Direction::STRAIGHT));
+          CHECK(tl.isGreen(0, dsf::Direction::LEFT));
         }
       }
     }
   }
-  // SUBCASE("Phase") {
-  //   /// This tests the phase.
-  //   /// GIVEN: A TrafficLight
-  //   /// WHEN: The phase is set to update after a green-red cycle
-  //   /// THEN: It's checked that the current gree-red cycle is not affected
-  //   /// and ultimately it's checked that on the next green-red cycle the phase is updated correctly
-  //   TrafficLight trafficLight{0};
-  //   trafficLight.setDelay(std::make_pair(5, 7));
-  //   trafficLight.setPhaseAfterCycle(6);
-
-  //   for (size_t i = 0; i < 12; ++i) {
-  //     trafficLight.increaseCounter();
-  //   }
-  //   CHECK_FALSE(trafficLight.isGreen());
-  //   trafficLight.increaseCounter();
-  //   CHECK_FALSE(trafficLight.isGreen());
-
-  //   trafficLight.setPhase(0);
-  //   CHECK(trafficLight.isGreen());
-
-  //   for (size_t i = 0; i < 12; ++i) {
-  //     trafficLight.increaseCounter();
-  //   }
-  //   CHECK(trafficLight.isGreen());
-  // }
-  // SUBCASE("Asymmetric traffic light") {
-  //   /// This tests the asymmetric traffic light.
-  //   /// GIVEN: A TrafficLight
-  //   /// WHEN: The asymmetric traffic light is set
-  //   /// THEN: The asymmetric traffic light is set correctly
-  //   TrafficLight trafficLight{0};
-  //   trafficLight.setDelay(std::make_pair(5, 3));
-  //   for (size_t i = 0; i < 8; ++i) {
-  //     if (i < 5) {
-  //       CHECK(trafficLight.isGreen());
-  //     } else {
-  //       CHECK_FALSE(trafficLight.isGreen());
-  //     }
-  //     trafficLight.increaseCounter();
-  //   }
-  //   CHECK(trafficLight.isGreen());
-  // }
-  // SUBCASE("Dynamic traffic light") {
-  //   GIVEN("A traffic ligth object with set delay") {
-  //     TrafficLight tl{0};
-  //     tl.setDelay(std::make_pair(5, 3));
-  //     WHEN("The delay is set with a green value smaller than the previous one") {
-  //       tl.increaseCounter();
-  //       tl.increaseCounter();
-  //       tl.increaseCounter();
-  //       tl.setDelay(std::make_pair(2, 3));
-  //       THEN("It is green for two cycles") {
-  //         CHECK(tl.isGreen());
-  //         tl.increaseCounter();
-  //         CHECK(tl.isGreen());
-  //         tl.increaseCounter();
-  //         CHECK_FALSE(tl.isGreen());
-  //       }
-  //     }
-  //     WHEN("The delay is set with a red value smaller than the previous one") {
-  //       tl.increaseCounter();
-  //       tl.increaseCounter();
-  //       tl.increaseCounter();
-  //       tl.increaseCounter();
-  //       tl.increaseCounter();
-  //       tl.setDelay(std::make_pair(1, 3));
-  //       THEN("It is red for one cycles") {
-  //         CHECK_FALSE(tl.isGreen());
-  //         tl.increaseCounter();
-  //         CHECK(tl.isGreen());
-  //       }
-  //     }
-  //   }
-  // }
 }
 
 TEST_CASE("Station") {
@@ -219,7 +148,7 @@ TEST_CASE("Station") {
     const std::string name = "S0001";
     GIVEN("A Station object") {
       WHEN("The Station is created using only an Id") {
-        Station station{id, managementTime};
+        dsf::Station station{id, managementTime};
         THEN("Parameters are set correctly") {
           CHECK_EQ(station.id(), id);
           CHECK_EQ(station.managementTime(), managementTime);
@@ -229,7 +158,7 @@ TEST_CASE("Station") {
         }
       }
       WHEN("The Station is created using an Id and coordinates") {
-        Station station{id, std::make_pair(lat, lon), managementTime};
+        dsf::Station station{id, std::make_pair(lat, lon), managementTime};
         THEN("Parameters are set correctly") {
           CHECK_EQ(station.id(), id);
           CHECK_EQ(station.managementTime(), managementTime);
@@ -239,7 +168,7 @@ TEST_CASE("Station") {
         }
       }
       WHEN("The Station is created using a copy constructor") {
-        Station base{id, std::make_pair(lat, lon), managementTime};
+        dsf::Station base{id, std::make_pair(lat, lon), managementTime};
         base.setCapacity(2);
         base.setTransportCapacity(3);
         base.setName(name);
@@ -258,7 +187,7 @@ TEST_CASE("Station") {
     constexpr dsf::Id id = 1;
     constexpr dsf::Delay managementTime = 2;
     GIVEN("A Station object") {
-      Station station{id, managementTime};
+      dsf::Station station{id, managementTime};
       WHEN("A train is enqueued") {
         station.enqueue(1, dsf::train_t::BUS);
         THEN("The train is enqueued correctly") { CHECK_EQ(station.dequeue(), 1); }
