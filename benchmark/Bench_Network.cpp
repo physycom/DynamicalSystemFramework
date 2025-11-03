4160917a421a1e985cb61f7531b26e5ab935181b
grufoony <gregorio_2000@hotmail.it>
2025-11-03 15:51:21 +0100

    Refactor some benhcs

---
+++ benchmark/Bench_Network.cpp
@@ -0,0 +1,78 @@
+#include "dsf/mobility/RoadNetwork.hpp"
+
+#include <filesystem>
+
+#include <benchmark/benchmark.h>
+
+static const auto DATA_FOLDER =
+    std::filesystem::path(__FILE__).parent_path().parent_path() / "test/data";
+
+static void BM_RoadNetwork_AddNode(benchmark::State& state) {
+  dsf::mobility::RoadNetwork network;
+  dsf::Id nodeId{0};
+  for (auto _ : state) {
+    network.addNode(nodeId++);
+  }
+}
+static void BM_RoadNetwork_AddEdge(benchmark::State& state) {
+  dsf::mobility::RoadNetwork network;
+  dsf::Id source{0}, target{1};
+  for (auto _ : state) {
+    network.addEdge(source, std::make_pair(source++, target++));
+  }
+}
+static void BM_RoadNetwork_CSVImport(benchmark::State& state) {
+  for (auto _ : state) {
+    dsf::mobility::RoadNetwork network;
+    network.importEdges((DATA_FOLDER / "postua_edges.csv").string());
+    network.importNodeProperties((DATA_FOLDER / "postua_nodes.csv").string());
+  }
+}
+static void BM_RoadNetwork_GeoJSONImport(benchmark::State& state) {
+  for (auto _ : state) {
+    dsf::mobility::RoadNetwork network;
+    network.importEdges((DATA_FOLDER / "postua_edges.geojson").string());
+    network.importNodeProperties((DATA_FOLDER / "postua_nodes.csv").string());
+  }
+}
+static void BM_RoadNetwork_NodesLooping(benchmark::State& state) {
+    dsf::mobility::RoadNetwork network;
+    network.importEdges((DATA_FOLDER / "forlì_edges.csv").string());
+    network.importNodeProperties((DATA_FOLDER / "forlì_nodes.csv").string());
+    for (auto _ : state) {
+        for (auto const& [id, node] : network.nodes()) {
+        benchmark::DoNotOptimize(id);
+        benchmark::DoNotOptimize(node);
+        }
+    }
+}
+static void BM_RoadNetwork_EdgesLooping(benchmark::State& state) {
+    dsf::mobility::RoadNetwork network;
+    network.importEdges((DATA_FOLDER / "forlì_edges.csv").string());
+    network.importNodeProperties((DATA_FOLDER / "forlì_nodes.csv").string());
+    for (auto _ : state) {
+        for (auto const& [id, edge] : network.edges()) {
+        benchmark::DoNotOptimize(id);
+        benchmark::DoNotOptimize(edge);
+        }
+    }
+}
+static void BM_RoadNetwork_ShortestPath(benchmark::State& state) {
+  dsf::mobility::RoadNetwork network;
+  network.importEdges((DATA_FOLDER / "forlì_edges.csv").string());
+  network.importNodeProperties((DATA_FOLDER / "forlì_nodes.csv").string());
+  auto itNode = network.nodes().cbegin();
+  for (auto _ : state) {
+    auto paths = network.allPathsTo(itNode->first, [](auto const& pEdge) { return pEdge->length(); });
+    ++itNode;
+  }
+}
+BENCHMARK(BM_RoadNetwork_AddNode);
+BENCHMARK(BM_RoadNetwork_AddEdge);
+BENCHMARK(BM_RoadNetwork_CSVImport);
+BENCHMARK(BM_RoadNetwork_GeoJSONImport);
+BENCHMARK(BM_RoadNetwork_NodesLooping);
+BENCHMARK(BM_RoadNetwork_EdgesLooping);
+BENCHMARK(BM_RoadNetwork_ShortestPath);
+
+BENCHMARK_MAIN();
